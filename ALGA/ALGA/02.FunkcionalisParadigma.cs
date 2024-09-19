182c2e44ed5c98731ee60d93f03185df6d682c7e
mateszabo02 <mateszabo02@stud.uni-obuda.hu>
2024-09-19 17:34:35 +0200

    Add files via upload

---
+++ ALGA/ALGA/02.FunkcionalisParadigma.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OE.ALGA.Paradigmak
+{
+    internal class FeltetelesFeladatTarolo<T> : FeladatTarolo<T> where T : IVegrehajthato
+    {
+        public FeltetelesFeladatTarolo(int meret) : base(meret)
+        {
+        }
+
+    }
+}
