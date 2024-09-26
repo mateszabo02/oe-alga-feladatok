0c76992af9904864c52ccc0c363c55c7a9ee8495
mateszabo02 <mateszabo02@stud.uni-obuda.hu>
2024-09-26 17:28:19 +0200

    Add files via upload

---
+++ ALGA/ALGA/03_Tomb.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OE.ALGA.Adatszerkezetek
+{
+    public class TombVerem<T> : Verem<T>
+    {
+        int n = 0;
+        T[] E;
+        public TombVerem(int meret) 
+        {
+            E = new T[meret];
+        }
+
+        public bool Ures
+        {
+            get { return n == 0; }
+        }
+
+        public T Felso()
+        {
+            if (!Ures)
+            {
+                return E[n - 1];
+            }
+            else { throw new NincsElemKivetel(); }
+
+        }
+
+        public void Verembe(T ertek)
+        {
+            if(n > E.Length)
+            {
+                throw new NincsHelyKivetel();
+            }
+            else
+            {
+                E[n] = ertek;
+                n++;
+            }
+        }
+
+        public T Verembol()
+        {
+            if (!Ures)
+            {
+                return E[--n];
+            }
+            else 
+            {
+                throw new NincsElemKivetel();
+            }
+        }
+    }
+    public class TombSor<T> : Sor<T>
+    {
+        T[] E;
+        int n = 0;
+        public TombSor(int meret)
+        {
+            E = new T[meret];
+        }
+        public bool Ures
+        {
+            get { return n == 0; }
+        }
+
+        public T Elso()
+        {
+            if (!Ures)
+            {
+                return E[n - 1];
+            }
+            else { throw new NincsElemKivetel(); }
+        }
+
+        public void Sorba(T ertek)
+        {
+           if(n > E.Length)
+            {
+                throw new NincsHelyKivetel();
+            }
+            else
+            {
+                E[n] = ertek;
+                n++;
+            }
+        }
+
+        public T Sorbol()
+        {
+            if (!Ures)
+            {
+                return E[--n];
+            }
+            else throw new NincsElemKivetel();
+        }
+    }
+    public class TombLista<T>
+    {
+        T[] E;
+        int n = 0;
+        public int Elemszam
+        {
+            get { return n; }
+        }
+    }
+
+}
---
+++ ALGA/ALGA/ADT.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OE.ALGA.Adatszerkezetek
+{
+    public interface Halmaz<T>
+    {
+        public void Beszur(T ertek);
+        public void Eleme(T ertek);
+        public void Torol(T ertek);
+        public void Bejar(Action<T> muvelet);
+    }
+    public interface Verem<T>
+    {
+        bool Ures { get; }
+        void Verembe(T ertek);
+        T Verembol();
+        T Felso();
+    }
+    public interface Sor<T>
+    {
+        bool Ures { get;}
+        void Sorba(T ertek);
+        T Sorbol();
+        T Elso();
+    }
+}
